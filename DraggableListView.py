2ae9a47476631691d8f36b76331cfced7a65cd83
Parham Oyan <parhamoyan@yahoo.com>
2023-04-09 01:51:49 +0200

    fix bug

--- DraggableListView.py
+++ DraggableListView.py
@@ -1,8 +1,9 @@
 import uuid
-from typing import Optional
+from dataclasses import dataclass
+from typing import Optional, List, Dict
 
-from PySide6.QtCore import QRect, QVariantAnimation, QPoint, QEasingCurve
-from PySide6.QtGui import QPaintEvent, QPainter, Qt, QResizeEvent, QWheelEvent, QMouseEvent
+from PySide6.QtCore import QRect, QVariantAnimation, QPoint, QEasingCurve, QParallelAnimationGroup
+from PySide6.QtGui import QPaintEvent, QPainter, Qt, QResizeEvent, QWheelEvent, QMouseEvent, QColor
 from PySide6.QtWidgets import QWidget
 
 from CustomScrollBar import CustomScrollBar
@@ -10,11 +11,18 @@ from Delegate import Delegate
 from models import Style, Item
 
 
+@dataclass
+class Index:
+    item: Item
+    item_style: Style
+    item_id: uuid.UUID
+
+
 class DraggableListView(QWidget):
     def __init__(self, parent: Optional[QWidget] = None) -> None:
         super().__init__(parent)
-        self.items = list()
-        self.items_style = list()
+        self.items_by_id: Dict[uuid.UUID, Index] = dict()
+        self.items_list: List[Index] = list()
         self.scroll_bar = CustomScrollBar(self)
         self.scroll_bar.valueChanged.connect(self.scroll_bar_value_changed)
         self.scroll_bar.raise_()
@@ -34,8 +42,9 @@ class DraggableListView(QWidget):
         self.dragged_item_row = None
         self.current_drop_row = None
         self.dragged_y_offset: float = 0
+        self.current_animated_items = list()
 
-        self.colors = [Qt.GlobalColor.darkMagenta, Qt.GlobalColor.darkRed, Qt.GlobalColor.darkBlue, Qt.GlobalColor.darkGreen, Qt.GlobalColor.darkGray]
+        self.colors = [QColor("#071E22"), QColor("#1D7874"), QColor("#679289"), QColor("#F4C095"), QColor("#EE2E31")]
         count = 5
         for i in range(count):
             self.addItem(i)
@@ -53,7 +62,7 @@ class DraggableListView(QWidget):
         _rect = QRect(0, 0, 8, self.rect().height())
         _rect.moveRight(self.rect().right())
         self.scroll_bar.setGeometry(_rect)
-        self.scroll_bar.setRange(0, 60 * len(self.items) - self.height())
+        self.scroll_bar.setRange(0, 60 * len(self.items_by_id) - self.height())
 
     def getIndexRect(self, index: int) -> QRect:
         _rect = QRect(0, index * 60 - self.scroll_bar.value(), self.width(), 60)
@@ -61,9 +70,11 @@ class DraggableListView(QWidget):
 
     def addItem(self, i) -> None:
         item = Item(self.colors[i])
+        item_style = Style(y_offset=0)
         item_id = uuid.uuid4()
-        self.items.append(item)
-        self.items_style.append(Style(y_offset=0))
+        index = Index(item, item_style, item_id)
+        self.items_list.append(index)
+        self.items_by_id[item_id] = index
         self.update()
 
     def indexAt(self, pos: QPoint) -> int:
@@ -71,10 +82,31 @@ class DraggableListView(QWidget):
         return int(index)
 
     def rowCount(self) -> int:
-        return len(self.items)
+        return len(self.items_by_id)
 
     def isRowValid(self, row: int) -> bool:
         return 0 <= row < self.rowCount()
+
+    def replace_item(self, from_index: int, to_index: int) -> None:
+        item_to_be_moved = self.items_by_id.pop(from_index)
+        item_style_to_be_moved = self.items_style.pop(from_index)
+        self.items_by_id.insert(to_index, item_to_be_moved)
+        self.items_style.insert(to_index, item_style_to_be_moved)
+
+    def mousePressEvent(self, event: QMouseEvent) -> None:
+        super().mousePressEvent(event)
+        index = self.indexAt(event.position())
+        if not 0 <= index < len(self.items_by_id):
+            return
+
+        if event.button() == Qt.LeftButton:
+            self.inner_drag_is_active = True
+            self.inner_drag_start_position = event.pos()
+            self.dragged_item = self.items_list[index].item
+            self.dragged_item_style = self.items_list[index].item_style
+            self.dragged_item_row = index
+            self.current_drop_row = index
+            self.current_shift_value = self.dragged_item_row
 
     def mouseMoveEvent(self, event: QMouseEvent) -> None:
         super().mouseMoveEvent(event)
@@ -88,72 +120,73 @@ class DraggableListView(QWidget):
             if new_drop_row >= self.rowCount():
                 new_drop_row = self.rowCount()-1
             if new_drop_row != self.current_drop_row:
-                direction = new_shift_value < 0
-                print(new_shift_value, self.current_shift_value)
-                self.current_shift_value = new_shift_value
-                self.current_drop_row = self.dragged_item_row + self.current_shift_value
-                animated_item_index = self.current_drop_row
-                item_style_to_be_animated = self.items_style[animated_item_index]
-                self.replace_animation = QVariantAnimation(self)
-                self.replace_animation.setStartValue(item_style_to_be_animated.y_offset)
-                if direction:
-                    self.replace_animation.setEndValue(item_style_to_be_animated.y_offset + 60)
+                previous_drop_row = self.current_drop_row
+                self.current_drop_row = new_drop_row
+                item_index: Index = self.items_list[self.current_drop_row]
+                item_id = item_index.item_id
+                self.animation = QVariantAnimation(self)
+                self.animation.setStartValue(item_index.item_style.y_offset)
+                self.animation.setDuration(200)
+
+                if self.current_drop_row < previous_drop_row:
+                    if self.current_drop_row >= self.dragged_item_row:
+                        row = self.current_drop_row + 1
+                        item_index: Index = self.items_list[row]
+                        item_id = item_index.item_id
+                        self.animation.setStartValue(item_index.item_style.y_offset)
+                        self.animation.setEndValue(0)
+                    else:
+                        row = self.current_drop_row
+                        item_index: Index = self.items_list[row]
+                        item_id = item_index.item_id
+                        self.animation.setStartValue(item_index.item_style.y_offset)
+                        self.animation.setEndValue(60)
                 else:
-                    self.replace_animation.setEndValue(item_style_to_be_animated.y_offset - 60)
-                self.replace_animation.setDuration(1000)
-                self.replace_animation.setEasingCurve(QEasingCurve.Type.InOutSine)
-                def update_value(new_value):
-                    item_style_to_be_animated.y_offset = new_value
+                    if self.current_drop_row <= self.dragged_item_row:
+                        row = self.current_drop_row - 1
+                        item_index: Index = self.items_list[row]
+                        item_id = item_index.item_id
+                        self.animation.setStartValue(item_index.item_style.y_offset)
+                        self.animation.setEndValue(0)
+                    else:
+                        row = self.current_drop_row
+                        item_index: Index = self.items_list[row]
+                        item_id = item_index.item_id
+                        self.animation.setStartValue(item_index.item_style.y_offset)
+                        self.animation.setEndValue(-60)
+
+                def value_changed(new_value):
+                    self.items_by_id[item_id].item_style.y_offset = new_value
                     self.update()
-                self.replace_animation.valueChanged.connect(update_value)
-                self.replace_animation.start()
+                self.animation.valueChanged.connect(value_changed)
+                self.animation.start()
+
             self.update()
-
-    def mousePressEvent(self, event: QMouseEvent) -> None:
-        super().mousePressEvent(event)
-        index = self.indexAt(event.position())
-        if not 0 <= index < len(self.items):
-            return
-        items = self.items
-        items_style = self.items_style
-        item = items[index]
-
-        if event.button() == Qt.LeftButton:
-            self.inner_drag_is_active = True
-            self.inner_drag_start_position = event.pos()
-            self.dragged_item = item
-            self.dragged_item_style = items_style[index]
-            self.dragged_item_row = index
-            self.current_drop_row = index
-            self.current_shift_value = self.dragged_item_row
 
     def mouseReleaseEvent(self, event: QMouseEvent) -> None:
         super().mouseReleaseEvent(event)
-        self.reorder_is_active = True
         destination_y = self.getIndexRect(self.current_drop_row).y()
         self.reorder_animation = QVariantAnimation(self)
         start_value = float(self.dragged_y_pos)
         end_value = float(destination_y)
         self.reorder_animation.setStartValue(start_value)
         self.reorder_animation.setEndValue(end_value)
-        self.reorder_animation.setDuration(200)
+        self.reorder_animation.setDuration(100)
         self.reorder_animation.setEasingCurve(QEasingCurve.Type.InOutSine)
 
         def update_value(new_value: float):
             self.dragged_y_pos = new_value
             self.update()
+
         self.reorder_animation.valueChanged.connect(update_value)
 
         def finished():
+            item_to_be_moved = self.items_list.pop(self.dragged_item_row)
+            for row in range(len(self.items_list)):
+                self.items_list[row].item_style.y_offset = 0
+            self.items_list.insert(self.current_drop_row, item_to_be_moved)
+
             self.inner_drag_is_active = False
-
-            self.replace_item(self.dragged_item_row, self.current_drop_row)
-
-            self.dragged_item_key = None
-            self.dragged_item_style.y_offset = 0
-            self.dragged_y_offset = 0
-            self.reorder_is_active = False
-
             self.inner_drag_start_position = QPoint()
             self.dragged_item = None
             self.dragged_item_style = None
@@ -161,21 +194,10 @@ class DraggableListView(QWidget):
             self.current_drop_row = None
             self.dragged_item_key = None
 
-            self.update()
         self.reorder_animation.finished.connect(finished)
         self.reorder_animation.start()
 
-    def replace_item(self, from_index: int, to_index: int) -> None:
-        item_to_be_moved = self.items.pop(from_index)
-        item_style_to_be_moved = self.items_style.pop(from_index)
-        if to_index > from_index:
-            for i in range(from_index, to_index):
-                self.items_style[i].y_offset = 0
-        else:
-            for i in range(to_index-1, from_index):
-                self.items_style[i].y_offset = 0
-        self.items.insert(to_index, item_to_be_moved)
-        self.items_style.insert(to_index, item_style_to_be_moved)
+        self.update()
 
     def paintEvent(self, event: QPaintEvent) -> None:
         painter = QPainter(self)
@@ -185,9 +207,9 @@ class DraggableListView(QWidget):
         painter.save()
         sum_offset = 0
         i = 0
-        for i in range(len(self.items)):
-            item = self.items[i]
-            item_style = self.items_style[i]
+        for i in range(len(self.items_by_id)):
+            item = self.items_list[i].item
+            item_style = self.items_list[i].item_style
             sum_offset += item_style.y_offset
             _rect = self.getIndexRect(i)
             real_rect = QRect(0, i * 60 - self.scroll_bar.value() + sum_offset, self.width(), 60)
